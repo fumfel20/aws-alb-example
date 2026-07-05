7f9821c4f21d031a839243d3c676a58ec53f9b45
kwojcik <fumfel@gmail.com>
2026-07-05 22:14:57 +0100

    add key

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,14 +6,20 @@ data "aws_vpc" "default" {
   default = true
 }
 
+locals {
+  public_key_path = pathexpand("~/.ssh/id_ed25519.pub")
+  has_public_key  = fileexists(local.public_key_path)
+}
+
 resource "aws_key_pair" "my_local_key" {
+  count      = local.has_public_key ? 1 : 0
   key_name   = "my-ssh-key"
-  public_key = file("~/.ssh/id_ed25519.pub")
+  public_key = file(local.public_key_path)
 }
 
 data "aws_subnets" "default" {
   filter {
-    name = "vpc-id"
+    name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
 }
@@ -64,9 +70,9 @@ resource "aws_security_group" "alb" {
 }
 
 resource "aws_instance" "web" {
-  ami = "ami-06468be052a4195a6"
-  instance_type = "t3.small"
-  key_name             = aws_key_pair.my_local_key.key_name
+  ami                    = "ami-06468be052a4195a6"
+  instance_type          = "t3.small"
+  key_name               = length(aws_key_pair.my_local_key) > 0 ? aws_key_pair.my_local_key[0].key_name : null
   vpc_security_group_ids = [aws_security_group.ec2.id]
   tags = {
     Name = "Docker-Ansible-Host"
@@ -74,29 +80,29 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_lb" "main" {
-  name = "aws-alb"
-  internal = false
+  name               = "aws-alb"
+  internal           = false
   load_balancer_type = "application"
-  security_groups = [aws_security_group.alb.id]
-  subnets = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.default.ids
   timeouts {
     create = "30m"
   }
 }
 
 resource "aws_lb_target_group" "main" {
-  name = "docker-targets"
-  port = 8080
+  name     = "docker-targets"
+  port     = 8080
   protocol = "HTTP"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id   = data.aws_vpc.default.id
 
   health_check {
-    path = "/"
-    port = "traffic-port"
-    healthy_threshold = 2
+    path                = "/"
+    port                = "traffic-port"
+    healthy_threshold   = 2
     unhealthy_threshold = 2
-    timeout = 3
-    interval = 10
+    timeout             = 3
+    interval            = 10
   }
 }
 
@@ -124,7 +130,7 @@ resource "aws_lb_listener" "front_end" {
 }
 
 resource "local_file" "ansible_inventory" {
-  content = <<EOF
+  content  = <<EOF
 [webservers]
 ${aws_instance.web.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa
 EOF
